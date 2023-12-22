db0d348c61c5ef169bbffde46e035fdf7820d583
peaga <pauloherbert@alu.ufc.br>
2023-12-22 09:22:42 -0300

    first project setup

---
+++ src/main/java/com/example/peagacatalog/PeagaCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.peagacatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeagaCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeagaCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/peagacatalog/PeagaCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.peagacatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeagaCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

de5f8359ec31cf31e1692070c44dbc66c44744e3
peaga <pauloherbert@alu.ufc.br>
2023-12-23 21:36:05 -0300

    finished Category's crud

--- src/main/java/com/example/peagacatalog/resources/CategoryResource.java
+++ src/main/java/com/example/peagacatalog/resources/CategoryResource.java
@@ -2,13 +2,19 @@ package com.example.peagacatalog.resources;
 
 import com.example.peagacatalog.dto.CategoryDTO;
 import com.example.peagacatalog.services.CategoryService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.HttpURLConnection;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,5 +29,22 @@ public class CategoryResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
         return ResponseEntity.ok(categoryService.findById(id));
+    }
+    @PostMapping
+    public ResponseEntity<CategoryDTO> create(@RequestBody CategoryDTO categoryDTO){
+        CategoryDTO dtoCreated = categoryService.create(categoryDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").build(dtoCreated.getId());
+        return ResponseEntity.created(uri).body(dtoCreated);
+    }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> update(@PathVariable Long id,@RequestBody CategoryDTO categoryDTO){
+        CategoryDTO dto = categoryService.update(id,categoryDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").build(dto.getId());
+        return ResponseEntity.status(HttpStatus.OK).location(uri).body(dto);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        categoryService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/example/peagacatalog/services/CategoryService.java
+++ src/main/java/com/example/peagacatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.example.peagacatalog.dto.CategoryDTO;
 import com.example.peagacatalog.entities.Category;
 import com.example.peagacatalog.repositories.CategoryRepository;
 import com.example.peagacatalog.services.exceptions.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,13 +21,26 @@ public class CategoryService {
         //List<CategoryDTO>  dtoList= categoryRepository.findAll().stream().map(CategoryDTO::new).collect(Collectors.toList()); //(c)-> new CategoryDTO(c)
         return dtoList;
     }
-    public CategoryDTO create(Category category){
-        return new CategoryDTO(categoryRepository.save(category));
+    public CategoryDTO create(CategoryDTO category){
+        return new CategoryDTO(categoryRepository.save(new Category(null,category.getName())));
     }
     public CategoryDTO findById(Long id){
         return new CategoryDTO(categoryRepository.findById(id).orElseThrow(()-> new EntityNotFoundException(" Category not found by id: "+id)));
     }
-   /* public void delete(Category){
-        categoryRepository.delete();
-    }*/
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO categoryDTO){
+        try{
+            Category entity = categoryRepository.getReferenceById(id);
+            entity.setName(categoryDTO.getName());
+            return new CategoryDTO(categoryRepository.save(entity));
+        }catch (jakarta.persistence.EntityNotFoundException e){
+            throw new EntityNotFoundException("Resource not found by id: "+id);
+        }
+    }
+   public void deleteById(Long id){
+        if(categoryRepository.existsById(id))
+            categoryRepository.deleteById(id);
+        else
+            throw new EntityNotFoundException("Resource not found id: "+id);
+    }
 }
